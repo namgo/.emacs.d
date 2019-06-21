a3d38a57974cb419b4d27b5e83efb8721d332b70
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-06-21 01:55:51 -0400

    Adding org-mode agenda config

--- init.d/init-org.el
+++ init.d/init-org.el
@@ -3,4 +3,10 @@
  '((python . t)))
 (setq org-startup-indented t)
 (define-key org-mode-map "\M-q" 'toggle-truncate-lines)
+(use-package org
+  :config
+  (setq org-agenda-files '("~/todo.org"))
+  :bind (("C-c a" . org-agenda)
+	 ("M-q" . toggle-truncate-lines))
+  )
 (provide 'init-org)
